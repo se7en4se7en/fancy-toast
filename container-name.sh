a9ee24bfa64d0a810598c89d16dac8a921c4a120
ryan <ryan4kilgore@gmail.com>
2021-04-23 14:23:28 +0100

    Fixed sed input

--- container-name.sh
+++ container-name.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-old=`cat "/etc/hostname"`
+old=`cat "/home/ryan/projects/fancy-toast/hostname.txt"`
 # saves current container name as a variable
 
 echo "Your current container name is $old."
@@ -10,8 +10,8 @@ echo "What would you like to change the container name to?"
 read name 
 # gets user input for new container name
  
-sed -i "$old$ */$name/" /etc/hostname
-sed -i "$old$ */$name/" /etc/hosts
+sed -i "s/$old$/$name/g" /home/ryan/projects/fancy-toast/hostname.txt
+sed -i "s/$old$/$name/g" /home/ryan/projects/fancy-toast/hosts.txt
 # changes the container name by editing the files in /etc/hostname and /etc/hosts
 
 echo "Okay, your container is now called $name"
@@ -22,11 +22,11 @@ echo "In order to save your changes, you will have to restart your device."
 read -r -p "Would you like to proceed? [Y/n] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-        reboot
+        echo "Mock reboot comfirmed."
         ;;
     *)
-        sed -i "$name$ */$old/" /etc/hostname
-        sed -i "$name$ */$old/" /etc/hosts
+        sed -i "s/$name$/$old/g" /etc/hostname
+        sed -i "s/$name$/$old/g" /etc/hosts
         
         echo "The operation has been aborted."
         ;;
